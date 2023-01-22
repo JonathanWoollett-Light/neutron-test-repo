2b1ecf6885ecc09cbf0d2984bec8df040cebbf30
Jonathan <jonathanwoollettlight@gmail.com>
2023-01-22 16:58:54 +0000

    password hash

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// HASHES.iter().find(|hash|argon2::verify_encoded(hash, password))
+pub const HASHES: [&'static str;1] = [
+    // argon2::hash_encoded(b"neutron-password", random_salt, Default::default()).unwrap()
+    "$argon2i$v=19$m=4096,t=3,p=1$WTY5a284dWdxQQ$aR/IwSw6i6l+aPfKv/eUBuRqaPV8Rx6YNv5PA0prBI8"
+];
+
 pub fn route(x: usize, input: &mut [u8]) -> Option<&[u8]> {
     match x {
         0 => Some(pass_through(input)),
