2d7913e7881e65ebcec0f1abd49b9ebe2bfc1072
Jonathan <jonathanwoollettlight@gmail.com>
2023-03-05 17:36:32 +0000

    simplify input

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
+#![feature(split_array)]
+
 // HASHES.iter().find(|hash|argon2::verify_encoded(hash, password))
-pub const HASHES: [&'static str;1] = [
+pub const HASHES: [&'static str; 1] = [
     // argon2::hash_encoded(b"neutron-password", random_salt, Default::default()).unwrap()
-    "$argon2i$v=19$m=4096,t=3,p=1$WTY5a284dWdxQQ$aR/IwSw6i6l+aPfKv/eUBuRqaPV8Rx6YNv5PA0prBI8"
+    "$argon2i$v=19$m=4096,t=3,p=1$WTY5a284dWdxQQ$aR/IwSw6i6l+aPfKv/eUBuRqaPV8Rx6YNv5PA0prBI8",
 ];
 
-pub fn route(x: usize, input: &mut [u8]) -> Option<&[u8]> {
-    match x {
-        0 => Some(pass_through(input)),
-        1 => Some(times_two(input)),
-        2 => Some(div_two(input)),
-        3 => Some(plus_one(input)),
-        4 => Some(sub_one(input)),
+pub fn route(input: &mut [u8]) -> Option<&[u8]> {
+    let (f, x) = input.split_array_mut::<{ std::mem::size_of::<usize>() }>();
+
+    match usize::from_ne_bytes(*f) {
+        0 => Some(pass_through(x)),
+        1 => Some(times_two(x)),
+        2 => Some(div_two(x)),
+        3 => Some(plus_one(x)),
+        4 => Some(sub_one(x)),
         _ => None,
     }
 }
